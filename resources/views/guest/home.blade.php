5c0e0d3483ffb45ebe56a096c750add73125c4e5
nico17022001 <nico17022001boolean@gmail.com>
2023-06-18 17:22:20 +0200

    non mi fa fare la migration

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -1,5 +1,21 @@
 @extends('layouts.guest')
 
 @section('content')
-    <h1>Home pubblica</h1>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="{{route('home')}}">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{route('login')}}">Login</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{route('register')}}">Registrati</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <h1>Home</h1>
 @endsection
