63d0801c8e72119a9656ce404900b3b3cbd2a4bc
nico17022001 <nico17022001boolean@gmail.com>
2023-06-18 18:02:24 +0200

    non riesco a vedere fare un dump di un post

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,8 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Post extends Model
 {
     use HasFactory;
+
+    public static function generateSlug($str){
+        $slug = Str::slug($str, '-');
+        $original_slug = $slug;
+
+        $slug_exixts = Post::where('slug', $slug)->first();
+        $c = 1;
+        while($slug_exists){
+            $slug = $original_slug . '-' . $c;
+            $slug_exists = Post::where('slug', $slug)->first();
+            $c++;
+        }
+        return $slug;
+    }
 }
---
+++ database/seeders/PostsTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Models\Post;
+
+class PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for($i = 0; $i < 2; $i++){
+            $new_post = new Post();
+            $new_post->title = $faker->sentence();
+            $new_post->slug = Post::generateSlug($new_post->title);
+            $new_post->description = $faker->text(1000);
+            $new_post->date = date('Y-m-d');
+            dump($new_post);
+        }
+    }
+}
